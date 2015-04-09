1faf5f2dd762cbb67c51917c98196ab21c369517
Graham Bevan <gbevan@csc.com>
2015-04-09 09:49:48 +0100

    wait for mongod to fork

--- 0.4.2/start-image.sh
+++ 0.4.2/start-image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 echo `date` "Starting MongoDB"
-/usr/bin/mongod --config /etc/mongod.conf --fork &
+/usr/bin/mongod --config /etc/mongod.conf --fork
 
 echo `date` "Starting SSHD"
 /usr/sbin/sshd -D -e
